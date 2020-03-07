bb56fcea24e86b975be3cc3aa10bdfccb8ddc72d
Guodong Xie <xiegd@gwu.edu>
2020-03-07 17:16:01 -0500

    init project

---
+++ src/main/java/LegendarySannin/App.java
@@ -0,0 +1,23 @@
+package LegendarySannin;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://google.com")).build();
+
+        client.sendAsync(request, BodyHandlers.ofString())
+         .thenApply(HttpResponse::body)
+         .thenAccept(System.out::println)
+         .join(); 
+    }
+}
